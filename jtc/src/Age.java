cb9b5c40fc0c6ef7fa2508eb0478b921ca82c945
Georg Fischer <dr.georg.fischer@gmail.com>
2017-11-10 09:05:10 +0100

    Age filled

--- jtc/src/Age.java
+++ jtc/src/Age.java
@@ -27,9 +27,36 @@ public class Age {
      * @return number of the month: 1 to 12
      */
     public static int getMonthNumber(String monthName) {
+        monthName = monthName.toLowerCase();
         int result = 0;
-        
-        
+        if (false) {
+        } else if (monthName.startsWith("jan")) {
+            result = 1;
+        } else if (monthName.startsWith("feb")) {
+            result = 2;
+        } else if (monthName.startsWith("mar")) {
+            result = 3;
+        } else if (monthName.startsWith("apr")) {
+            result = 4;
+        } else if (monthName.startsWith("m"  )) {
+            result = 5;
+        } else if (monthName.startsWith("jun")) {
+            result = 6;
+        } else if (monthName.startsWith("jul")) {
+            result = 7;
+        } else if (monthName.startsWith("aug")) {
+            result = 8;
+        } else if (monthName.startsWith("sep")) {
+            result = 9;
+        } else if (monthName.startsWith("o"  )) {
+            result = 10;
+        } else if (monthName.startsWith("nov")) {
+            result = 11;
+        } else if (monthName.startsWith("de ")) {
+            result = 12;
+        } else {
+            result = 0;
+        }
         return result;
     } // getMonthNumber
 
@@ -41,7 +68,10 @@ public class Age {
         if (args.length == 0) { // set default arguments
             args = new String[] { "29", "Jun", "1947", "" };
         } // set default arguments
-
+        int day    = Integer.parseInt(args[0]);
+        int month  = getMonthNumber  (args[1]);
+        int year   = Integer.parseInt(args[2]);
+        System.out.println("day=" + day + ", month=" + month + ", year=" + year);
     } // main
 
 } // Age
