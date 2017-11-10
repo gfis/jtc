67dd7848f209c58fd36f7ec62d585a8880ed8930
Georg Fischer <dr.georg.fischer@gmail.com>
2017-11-10 07:48:56 +0100

    new Month

---
+++ jtc/src/Month.java
@@ -0,0 +1,78 @@
+/* Convert the number of a month to its name
+ * Java Training Course example file
+ * 2017-11-10, Georg Fischer
+ *
+ * Copyright 2017 Georg Fischer &lt;dr.georg.fischer at gmail.com>&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+
+/**
+ * Convert the number of a month to its name
+ * @author Georg Fischer &lt;dr.georg.fischer at gmail.com>&gt;
+ */
+public class Month {
+
+    /**
+     * Test program
+     *
+     * @param args commandline arguments: the number of a month
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) { // set default arguments
+            args = new String[]{"6", "", "", ""};
+        } // set default arguments
+        int num = Integer.parseInt(args[0]);
+        String monthName = "";
+        switch (num) {
+            case 1:
+                monthName = "Januari";
+                break;
+            case 2:
+                monthName = "Februari";
+                break;
+            case 3:
+                monthName = "Maret";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "Mei";
+                break;
+            case 6:
+                monthName = "Juni";
+                break;
+            case 7:
+                monthName = "Juli";
+                break;
+            case 8:
+                monthName = "Agustus";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "Oktober";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "Desember";
+                break;
+        }
+        System.out.println(monthName);
+
+    } // main
+
+} // Month
