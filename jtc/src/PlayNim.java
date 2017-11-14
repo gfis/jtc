d6909f42b2ca3aa06da575875d77cb9ab2c8162c
Georg Fischer <dr.georg.fischer@gmail.com>
2017-11-14 10:10:01 +0100

    more 14.2

--- jtc/src/PlayNim.java
+++ jtc/src/PlayNim.java
@@ -17,12 +17,12 @@
  * limitations under the License.
  */
 import java.util.Scanner;
+
 /**
- * Plays a simple Nim misère game.
- * The machine writes the rules, and then
- * presents a number of sticks to the human player, who begins.
- * The human takes 1, 2 or 3 sticks, and than the machine does the same.
- * The one who has to take the last stick will loose.
+ * Plays a simple Nim misère game. The machine writes the rules, and then
+ * presents a number of sticks to the human player, who begins. The human takes
+ * 1, 2 or 3 sticks, and than the machine does the same. The one who has to take
+ * the last stick will loose.
  *
  * @author Georg Fischer &lt;dr.georg.fischer at gmail.com&gt;
  */
@@ -30,19 +30,45 @@ public class PlayNim {
 
     /**
      * Test program
-     *  Interactive input on the commandline: the number of sticks chosen by the human.
      */
     public static void main(String[] args) {
         // create a scanner so we can read the command-line input
         Scanner scanner = new Scanner(System.in);
         //  prompt for the user's name
-        while (true) {
+        int machineTaken = 0;
+        int noSticks = 19;
+        int minTake = 1;
+        int maxTake = 3;
+        while (noSticks >= 0) {
+            System.out.println("There are " + noSticks + " left.");
             System.out.print("How many sticks do you take? ");
             int humanTaken = scanner.nextInt();
-            System.out.println("You take " + humanTaken + ".");
-            System.out.println("I take "   + machineTaken + ".");
-            
-        } // while input
+            if (humanTaken < minTake || humanTaken > maxTake) {
+                System.out.println("** You must take between " + minTake + " and " + maxTake + " sticks!");
+            } else { // proper humanTaken
+                noSticks -= humanTaken;
+                System.out.println(noSticks + " are left.");
+                if (noSticks == 1) { // machine looses
+                    System.out.println("Good for you! You have won!");
+                    noSticks = -1;
+                } else { // machine not yet lost
+                    if ((noSticks - 1) % 4 == 0) { // human was clever: machine tries randomly
+                        machineTaken = (new Double(Math.random() * 3.0 + 1.0)).intValue(); // >= 0.0 and < 1.0 -> 1..3
+                    } else { // human did not reach a magic number: machine takes it and wins
+                        int nextLowerMagic = ((noSticks - 1) / 4) * 4 + 1;
+                        machineTaken = noSticks - nextLowerMagic;
+                        System.out.print("I will win! ");
+                    } // machine wins
+                    System.out.println("I take " + machineTaken + ".");
+                    noSticks -= machineTaken;
+                    // now decide wether we reached the end
+                    if (noSticks == 1) {
+                        System.out.println("Bad for you. I have won.");
+                        noSticks = -1;
+                    }
+                } // machine not yet lost
+            } // proper humanTaken
+        } // while noSticks >= 0
     } // main
 
 } // PlayNim
