32085a9ae508373e3c8b38a442e4472b327d886a
Georg Fischer <dr.georg.fischer@gmail.com>
2017-11-23 01:30:23 +0100

    WalkFileTree1

---
+++ jtc/src/org/teherba/jtc/file/WalkFileTree.java
@@ -0,0 +1,80 @@
+/* 
+ * Java Training Course example file
+ * 2017-11-23, Georg Fischer
+ *
+ * Copyright 2017 Georg Fischer &lt;dr.georg.fischer at gmail.com>&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. */
+package org.teherba.jtc.file;
+
+/**
+ *
+ * @author Georg Fischer &lt;dr.georg.fischer at gmail.com>&gt;
+ */
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+public class WalkFileTree 
+            extends SimpleFileVisitor<Path> {
+
+        // Print information about
+        // each type of file.
+        @Override
+        public FileVisitResult visitFile(Path file,
+                BasicFileAttributes attr) {
+            if (attr.isSymbolicLink()) {
+                System.out.format("Symbolic link: %s ", file);
+            } else if (attr.isRegularFile()) {
+                System.out.format("Regular file: %s ", file);
+            } else {
+                System.out.format("Other: %s ", file);
+            }
+            System.out.println("(" + attr.size() + "bytes)");
+            return FileVisitResult.CONTINUE;
+        }
+
+        // Print each directory visited.
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir,
+                IOException exc) {
+            System.out.format("Directory: %s%n", dir);
+            return FileVisitResult.CONTINUE;
+        }
+
+        // If there is some error accessing
+        // the file, let the user know.
+        // If you don't override this method
+        // and an error occurs, an IOException 
+        // is thrown.
+        @Override
+        public FileVisitResult visitFileFailed(Path file,
+                IOException exc) {
+            System.err.println(exc);
+            return FileVisitResult.CONTINUE;
+        }
+
+    /**
+     * Test program
+     *
+     * @param args commandline arguments
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) { // set default arguments
+            args = new String[]{".", "", "", ""};
+        } // set default arguments
+
+    } // main
+
+} // WalkFileTree
